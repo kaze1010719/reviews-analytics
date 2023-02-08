928c375376ee65be4f7fd8ae8ca6cd2b26f3e16d
kaze1010719 <43249684+kaze1010719@users.noreply.github.com>
2023-02-08 14:15:37 +0800

    add word count function

--- read.py
+++ read.py
@@ -13,6 +13,7 @@ print(data[0])
 print('----------------')
 print(data[1])
 
+
 sum_len = 0
 for d in data:
 	sum_len += len(d) #d是指字母數，the是三個字
@@ -23,7 +24,7 @@ print('留言的平均長度為', sum_len / len(data))
 new = []
 for d in data:
 	if len(d) < 100:
-		new.append(d)
+		new.append(d) 
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
@@ -43,3 +44,32 @@ bad = ['bad' in d for d in data]
 bad = []
 for d in data:
 	bad.append('bad' in d) # bad in d 是 True/False
+
+
+# 文字記數
+wc = {} # word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 # 新增新的key進wc字典
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])			
+
+print(len(wc))
+print(wc['Allen'])
+
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+		break
+	if word in wc:	
+		print(word, '出現過的字數為: ', wc[word])
+	else:
+		print('這個字沒有出現過喔!')	
+
+print('感謝使用本查詢功能')
